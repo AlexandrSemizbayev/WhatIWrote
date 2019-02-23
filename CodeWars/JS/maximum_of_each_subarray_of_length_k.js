865a178b0cac3e0dda5402d1986c0d75df0b957a
Alexandr <bayernummer1@gmail.com>
2019-02-23 19:00:42 +0600

    Create maximum_of_each_subarray_of_length_k.js

---
+++ CodeWars/JS/maximum_of_each_subarray_of_length_k.js
@@ -0,0 +1,31 @@
+/*
+This problem was asked by Google.
+Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each
+subarray of length k.
+For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+10 = max(10, 5, 2)
+7 = max(5, 2, 7)
+8 = max(2, 7, 8)
+8 = max(7, 8, 7)
+Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results.
+You can simply print them out as you compute them.
+*/
+
+
+
+
+
+	var arr = [7,5,10,7,5];
+	var k = 3;
+	for(var len = 0;len<=arr.length-k;len++){
+		var max = 0;
+		if(arr.length>len+k-1){
+			for(var j=len; j<len+k;j++){
+				if(arr[j]>max){
+					max=arr[j]
+				}
+			}	
+		}
+	console.log(max)
+	}
+
